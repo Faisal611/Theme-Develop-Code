5d12ec560549749c052dafcaf9124709ddbefccd
else-dev <skfaisal1997@gmail.com>
2022-10-13 16:07:45 +0600

    publish

--- explode-implode.php
+++ explode-implode.php
@@ -1,22 +1,4 @@
-<?php
 
-/*--
- 1. explode -> explode function holo ( content -> array ) te Converte kora hoy !
- 2 .implode -> implode function holo ( array -> staing ) te converte kora hoy !
- 3 . Search option
----*/
-
-
-function readmore($count) {
-
-    $content    = get_the_content();
-    $array 	    = explode('',$content);
-    $lassArray  = array_slice($array,0,$count);
-
-    echo implode('',$lassArray);
-}
-
-?>
 <!--Font Page-->
 <p class="post-text"> <?php readmore(35) ?></p>
 
