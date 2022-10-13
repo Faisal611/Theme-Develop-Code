d8272e7764bf46cb1e90500821ec4c4b513933c7
else-dev <skfaisal1997@gmail.com>
2022-10-13 16:05:32 +0600

    publish

--- explode-implode.php
+++ explode-implode.php
@@ -20,3 +20,8 @@ function readmore($count) {
 <!--Font Page-->
 <p class="post-text"> <?php readmore(35) ?></p>
 
+<!---- Search option ---->
+<form action="<?php echo home_url(); ?>" method="get">
+    <input type="text" name="s" placeholder="Type here">
+    <button type="submit"><i class="ion-chevron-right"></i></button>
+</form>
